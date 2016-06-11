fc511a325f51452d3e7cf504f73a6de2d18cb2ba
yupengyp@gmail.com <yupengyp@gmail.com>
2016-06-11 19:27:14 +0800

    modify dir

--- src/flexible.js
+++ src/flexible.js
@@ -7,20 +7,22 @@
 		var handler	 	= null;
 
 		var flexible = tcf.flexible || (tcf.flexible = {});
-    
-		if (metaEl) {
-	        console.warn('将根据已有的meta标签来设置缩放比例');
+
+    	//将根据已有的meta标签来设置缩放比例
+		if (metaEl) { 
 	        var match = metaEl.getAttribute('content').match(/initial\-scale=([\d\.]+)/);
 	        if (match) {
 	            scale = parseFloat(match[1]);
 	            dpr = parseInt(1 / scale);
 	        }
 	    }
-
+	    //如果没有设置，那么会自动添加meta viewport的设置
 	    if (!dpr && !scale) {
 	        var isAndroid 		 = win.navigator.appVersion.match(/android/gi);
 	        var isIPhone 		 = win.navigator.appVersion.match(/iphone/gi);
-	        var isliebao 		 = win.navigator.appVersion.match(/LieBaoFast/gi);
+	        var isLiebao 		 = win.navigator.appVersion.match(/LieBao/gi);
+	        var isSogou 		 = win.navigator.appVersion.match(/Sogou/gi);
+	        var clsName 		 = String(isIPhone||isLiebao||isSogou||isAndroid||'').toLowerCase();
 	        var devicePixelRatio = Math.floor(win.devicePixelRatio)||1;
 	        if (isIPhone) {
 	            if (devicePixelRatio >= 3 && (!dpr || dpr >= 3)) {                
@@ -31,25 +33,22 @@
 	                dpr = 1;
 	            }
 
-	            doc.body.classList.add('iphone');
-
-	        }else if(isliebao){
+	        }else if(isLiebao || isSogou){
 	        	dpr = 1;
-	        	doc.body.classList.add('liebao');
-	        } else {
-	
-	            dpr = devicePixelRatio;
-	            doc.body.classList.add('android');
+	        }else {
+	            dpr = devicePixelRatio;   
 	        }
+
+	        doc.body.classList.add(clsName);
 	        scale = 1 / dpr;
 	    }
 
-		docEl.setAttribute('data-dpr', dpr);
+		
 
 		if (!metaEl) {
-	        metaEl = doc.createElement('meta');
+			metaEl = doc.createElement('meta');
 	        metaEl.setAttribute('name', 'viewport');
-	        metaEl.setAttribute('content', 'initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=no');
+	        metaEl.setAttribute('content', 'initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale +',user-scalable=no');
 	        if (docEl.firstElementChild) {
 	            docEl.firstElementChild.appendChild(metaEl);
 	        } else {
@@ -59,14 +58,17 @@
 	        }
 	    }
 
+	    docEl.setAttribute('data-dpr', dpr);
+
 		var setFontSize = function(){
-			var width = innerWidth;
+			var width = docEl.getBoundingClientRect().width;
 	        if (width / dpr > 540) {
 	            width = 540 * dpr;
 	        }
 	        var rem = width / 10;
+	        rem = Math.max(rem,32);
 			docEl.style.fontSize =  rem+'px';
-			flexible.width = innerWidth;
+			flexible.width = width;
 			flexible.rem = win.rem = rem;
 		};
 
@@ -85,14 +87,15 @@
 	    }, false);
 
 	    if (doc.readyState === 'complete') {
+	    	
 	        doc.body.style.fontSize = 12 * dpr + 'px';
 	    } else {
 	        doc.addEventListener('DOMContentLoaded', function(e) {
 	            doc.body.style.fontSize = 12 * dpr + 'px';
+
 	        }, false);
 	    }
     
-
     	setFontSize();
 
 	    flexible.dpr = win.dpr = dpr;
